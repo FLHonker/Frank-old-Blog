110465a5d925f364d80c2936e708938faed139a7
FLHonker <frankliuceo@163.com>
2018-07-01 10:48:01 +0800

    krmarkdown

--- publish.sh
+++ publish.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 # 一键部署github-pages，包括更新bundle依赖、运行jekyll服务、git提交与push。
-# 保存当前目录
-currentDir=$PWD
-echo "Start to publish..."
-# 切换到FLHonker.github.io目录
-cd /home/frank/Study/FLHonker.github.io/
+# Usage: ./publish.sh message [-u]
+
 if [ -z "$1" ]
 then
     echo "Usage: $0 message [-u]"
+    exit 1
 fi
+
+# 保存当前目录
+currentDir=$PWD
+# 切换到FLHonker.github.io目录
+cd /home/frank/Study/FLHonker.github.io/
+echo "Start to publish..."
+
 # bundle更新与生成
 if [ "$2" == "-u" ]; then
     bundle update
@@ -23,3 +28,5 @@ cd $currentDir
 echo ""
 # echo -e "\033[42;37m Frank Blog update online! \033[0m"
 echo "Frank Blog update online!"
+
+exit 0
